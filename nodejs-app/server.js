8df754f0515175545349c0e33b5ac9ac62265d53
quincycs <2924643+quincycs@users.noreply.github.com>
2020-12-09 22:47:17 +0000

    added nodejs.  can run in devserver

---
+++ nodejs-app/server.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const express = require('express');
+const DynamoDB = require('aws-sdk/clients/dynamodb');
+
+/*
+ * Environment variables usages
+ */
+const PORT = parseInt(process.env.PORT);
+const HOST = process.env.HOST;
+const REGION = process.env.AWS_DEFAULT_REGION;
+const dbTable = process.env.dbTableName;
+
+const db = new DynamoDB({
+  apiVersion: '2012-08-10',
+  region: REGION
+});
+const app = express();
+
+app.get('/', (req, res) => {
+  console.log('hi!');
+  const params = {
+    Key: {
+      id: {
+        S: "hello world"
+      }
+    },
+    TableName: dbTable
+  };
+  db.getItem(params, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.send('ERROR: ' + err);
+    }
+    else {
+      console.log(data);
+      res.send('SUCCESS: ' + JSON.stringify(data));
+    }
+  });
+});
+
+app.listen(PORT, HOST, () => {
+  console.log(`Listening on ${PORT}`);
+});
